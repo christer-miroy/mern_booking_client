f759fa7990fec8bad61ba7dccf4ecea689949bc6
Christer Miroy <chrstrzm.mry@gmail.com>
2023-12-11 01:51:27 +0800

    add more pList

--- src/components/propertyList/PropertyList.jsx
+++ src/components/propertyList/PropertyList.jsx
@@ -24,6 +24,20 @@ const PropertyList = () => {
                 <p>123 hostels</p>
             </div>
         </div>
+        <div className="pListItem">
+            <img src="https://images.unsplash.com/photo-1520250497591-112f2f40a3f4?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjN8fGJlYWNoJTIwcmVzb3J0JTIwJTI2JTIwc3BhfGVufDB8fDB8fHww" alt="" className="pListImg" />
+            <div className="pListTitles">
+                <h3>Resorts</h3>
+                <p>52 resorts</p>
+            </div>
+        </div>
+        <div className="pListItem">
+            <img src="https://plus.unsplash.com/premium_photo-1678286771082-4de8d1e4b649?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fGJlYWNoJTIwdmlsbGFzfGVufDB8fDB8fHww" alt="" className="pListImg" />
+            <div className="pListTitles">
+                <h3>Villas</h3>
+                <p>98 villas</p>
+            </div>
+        </div>
     </div>
   )
 }
