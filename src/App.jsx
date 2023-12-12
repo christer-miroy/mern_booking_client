61a43d740c234d834f1228cfb3ff6eee6c8cd907
Christer Miroy <chrstrzm.mry@gmail.com>
2023-12-12 15:49:31 +0800

    hotel page design done

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Listings from "./pages/listings/Listings";
-import Property from "./pages/property/Property";
 import Navbar from "./components/navbar/Navbar";
+import Hotel from "./pages/hotel/Hotel";
 
 function App() {
   return (
@@ -11,7 +11,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/hotels" element={<Listings />} />
-      <Route path="/hotels/:id" element={<Property />} />
+      <Route path="/hotels/:id" element={<Hotel />} />
     </Routes>
     </BrowserRouter>
   );
